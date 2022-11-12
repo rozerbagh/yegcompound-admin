f4bb87fda5a52126afac42152bf4241e0a74d55b
rozerbagh <rozerbagh456@gmail.com>
2022-11-12 22:11:49 +0530

    the public/index.html has been changed

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-// const baseURL = 'http://143.244.128.159:3003/api/v1';
-const baseURL = 'http://localhost:3003/api/v1';
+const baseURL = 'http://143.244.128.159:3003/api/v1';
+// const baseURL = 'http://localhost:3003/api/v1';
 export const login = `${baseURL}/user/login`;
 export const signup = `${baseURL}/user/signup`;
 export const createOrders = `${baseURL}/app/order/add`;
